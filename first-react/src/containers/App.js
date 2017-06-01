37445b952228339f15313cb2d85b2a0888e31d12
sreeragchalava <sreeragchalava@gmail.com>
2017-06-01 22:12:33 +0530

    react-setup

---
+++ first-react/src/containers/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+//import '../App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome to React</h2>
+        </div>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
